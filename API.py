5188fd0f7a22fc64f9b8bb8d1f39474379b07f30
ChoDory <wayou0822@gmail.com>
2024-01-15 13:39:20 +0900

    Initial commit

---
+++ API.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify, request
+import json
+import csv
+
+app = Flask(__name__)
+
+@app.route('/get_pod1', methods=['GET'])
+def get_pod1_data():
+    with open('pod1_info.json', 'r') as json_file:
+        data = json.load(json_file)
+    return jsonify(data)
+
+@app.route('/get_pod2', methods=['GET'])
+def get_pod2_data():
+    with open('pod2_info.json', 'r') as json_file:
+        data = json.load(json_file)
+    return jsonify(data)
+
+@app.route('/receive_data_pod1', methods=['POST'])
+def receive_data_pod1():
+    data = request.json  # JSON 데이터를 파싱
+
+    # CSV 파일명 정의
+    csv_file_name = 'received_data_pod1.csv'
+
+    # CSV 파일 열기
+    with open(csv_file_name, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        # CSV 파일이 비어있다면, 헤더를 작성
+        if csvfile.tell() == 0:
+            writer.writerow(data.keys())
+
+        # 데이터 쓰기
+        writer.writerow(data.values())
+    # 성공 메시지 반환
+    return jsonify({"status": "success", "message": "Data received successfully."})
+
+@app.route('/receive_data_pod2', methods=['POST'])
+def receive_data_pod2():
+    data = request.json  # JSON 데이터를 파싱
+    
+    csv_file_name = 'received_data_pod2.csv'
+
+    # CSV 파일 열기
+    with open(csv_file_name, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        # CSV 파일이 비어있다면, 헤더를 작성
+        if csvfile.tell() == 0:
+            writer.writerow(data.keys())
+
+        # 데이터 쓰기
+        writer.writerow(data.values())
+    # 성공 메시지 반환
+    return jsonify({"status": "success", "message": "Data received successfully."})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
---
+++ cordinator.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify
+import requests
+import json
+
+def recommend_cpu_frequency(task_size_range):
+    range_size = task_size_range[1] - task_size_range[0]
+    
+    if range_size <= 1000:
+        return 700000  
+    elif range_size <= 5000:
+        return 1200000  
+    else:
+        return 1800000 
+
+def get_task_sizes():
+    start1 = 1
+    end1 =  1000
+    start2 = 1001
+    end2 = 3000
+    return (start1, end1), (start2, end2)
+
+# 작업 크기 범위 입력받기
+task_range1, task_range2 = get_task_sizes()
+
+pod1_task_size = task_range1[1] - task_range1[0] + 1
+pod2_task_siee = task_range2[1] - task_range2[0] + 1
+
+# 각 범위에 대한 권장 CPU 주파수 계산
+recommended_freq1 = recommend_cpu_frequency(task_range1)  # 첫 번째 범위 사용
+recommended_freq2 = recommend_cpu_frequency(task_range2)  # 두 번째 범위 사용
+
+print(f"Task Range(pod1) {task_range1[0]}~{task_range1[1]} recommended CPUfreq: {recommended_freq1}MHz")
+print(f"Task Range(pod2) {task_range2[0]}~{task_range2[1]} recommended CPUfreq: {recommended_freq2}MHz")
+
+# 각 파드 정보를 JSON 형식으로 생성
+pod1_info = {
+    "pod_name": "pod1",
+    "task_size_range": task_range1,
+    "task_size": pod1_task_size,
+    "recommended_cpu_frequency": recommended_freq1
+}
+
+pod2_info = {
+    "pod_name": "pod2",
+    "task_size_range": task_range2,
+    "task_size":pod2_task_siee,
+    "recommended_cpu_frequency": recommended_freq2
+}
+
+# pod1_info를 pod1_info.json 파일에 저장
+with open('pod1_info.json', 'w') as json_file:
+    json.dump(pod1_info, json_file, indent=4)
+
+# pod2_info를 pod2_info.json 파일에 저장
+with open('pod2_info.json', 'w') as json_file:
+    json.dump(pod2_info, json_file, indent=4)
+
